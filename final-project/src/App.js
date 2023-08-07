50091de09f6da21e8933dffbcafd94a64c14cd75
jhnlee817 <yobjohn2@yahoo.com>
2023-08-07 14:30:22 -0500

    fix react errors

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          testing
+          re-testing testing
         </a>
       </header>
     </div>
