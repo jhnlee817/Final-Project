c6e682124ecff3736b2aa1e8d19b421dc9aab071
jhnlee817 <yobjohn2@yahoo.com>
2023-08-07 14:34:48 -0500

    last test before start

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          re-testing testing
+          last test
         </a>
       </header>
     </div>
