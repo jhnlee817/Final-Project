dd284c138407346c0b731db61faffe1d48513bea
jhnlee817 <yobjohn2@yahoo.com>
2023-08-09 14:01:14 -0500

    add particle effects

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -3,12 +3,14 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ParticlesBg type="cobweb" bg={true} />
         <Navigation />
         <Logo />
         <ImageLinkForm />
