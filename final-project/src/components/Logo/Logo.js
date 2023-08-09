cae91b49e47e0fdb4fe8a356043ef73bfca7233d
jhnlee817 <yobjohn2@yahoo.com>
2023-08-09 13:52:19 -0500

    make imagelinkform with some text filler

--- final-project/src/components/Logo/Logo.js
+++ final-project/src/components/Logo/Logo.js
@@ -5,9 +5,9 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0' style={{ maxWidth: '300px'}}>
+        <div className='ma4 mt0' style={{ maxHeight: '100px', maxWidth: '100px'}}>
             <Tilt>
-                <div className='tilt br2 shadow-2'  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px', maxWidth: '300px'}}>
+                <div className='tilt br2 shadow-2'  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px', maxWidth: '100px'}}>
                     <img style={{paddingTop:'5px'}} alt='logo' src={brain}></img>
                 </div>
             </Tilt>
