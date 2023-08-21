30f097b7769b820526b985da74adecfc95184c70
jhnlee817 <yobjohn2@yahoo.com>
2023-08-21 14:57:50 -0500

    try to figure out old clarifai with new clarifai

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg';
 import './App.css';
+
+// const app = new Clarifai.App({
+//   apiKey: 'a64ae5a75e57499cbfd5ebb9eeb080bc'
+// });
 
 class App extends Component {
   constructor() {
@@ -20,6 +26,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    // app.models.predict("a64ae5a75e57499cbfd5ebb9eeb080bc", "https://samples.clarifai.com/face-det.jpg").then (
+    //   function(response) {
+    //     console.log(response);
+    //   },
+    //   function(err) {
+
+    //   }
+    // );
   }
 
 
@@ -31,7 +45,7 @@ class App extends Component {
         <Logo />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         <Rank />
-        {/* {<FaceRecognition />} */}
+        {<FaceRecognition />}
       </div>
     )
   }
