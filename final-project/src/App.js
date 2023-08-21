8f105896ebb5ff56aa119f189202408e0a4863f9
jhnlee817 <yobjohn2@yahoo.com>
2023-08-21 14:14:28 -0500

    make detectors for detect button and search bar

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -7,13 +7,29 @@ import ParticlesBg from 'particles-bg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+
   render() {
     return (
       <div className="App">
         <ParticlesBg type="cobweb" bg={true} />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         <Rank />
         {/* {<FaceRecognition />} */}
       </div>
